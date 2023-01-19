7bbcf61e630f584c72f35226b1e1d92b4a461c64
socry <leonardosbo02@gmail.com>
2023-01-19 05:12:48 -0300

    Add function to simulate a rock paper scissors game

--- script.js
+++ script.js
@@ -13,3 +13,40 @@ let playerSelection = () => {
     }
     return answer;
 }
+
+let playRound = (playerSelection, computerSelection) =>{
+    
+    if(playerSelection === 'rock'){
+        if(computerSelection === 'rock'){
+            return "Draw!";
+        }
+        else if(computerSelection === 'scissors'){
+            return "You Win! Rock beats Scissors";
+        }
+        else{
+            return "You Lose! Paper beats Rock";
+        }
+    }
+    else if(playerSelection === 'paper'){
+        if(computerSelection === 'paper'){
+            return "Draw!";
+        }
+        else if(computerSelection === 'rock'){
+            return "You Win! Paper beats Rock";
+        }
+        else{
+            return "You Lose! Scissors beats Paper";
+        }
+    }
+    else{
+        if(computerSelection === 'scissors'){
+            return "Draw!";
+        }
+        else if(computerSelection === 'paper'){
+            return "You Win! Scissors beats Paper";
+        }
+        else{
+            return "You Lose! Rock beats Scissors";
+        }
+    }
+}
