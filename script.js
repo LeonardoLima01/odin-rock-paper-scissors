035e6eb1036344333811c193925a98f31ad5f9cd
socry <leonardosbo02@gmail.com>
2023-01-19 05:12:02 -0300

    Add function to ask user for input

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ let getComputerChoice = () =>{
 
     return options[choice];
 }
+
+let playerSelection = () => {
+    let answer = 'a';
+
+    while (answer != 'rock' && answer != 'paper' && answer != 'scissors'){
+        answer = prompt("Rock, Paper or Scissors?").toLowerCase()
+    }
+    return answer;
+}
